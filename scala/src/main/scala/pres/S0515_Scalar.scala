797ec3c031398c49a5ce55b8b8412ede5c73b268
adamw <adam@warski.org>
2024-01-30 14:46:02 +0100

    WIP

---
+++ scala/src/main/scala/pres/S0515_Scalar.scala
@@ -0,0 +1,12 @@
+package pres
+
+import java.time.LocalDate
+
+object S0515_Scalar:
+  val Scalar = "https://www.scalar-conf.com"
+
+  val Workshops = LocalDate.of(2024, 3, 20)
+  val From = LocalDate.of(2024, 3, 21)
+  val To = LocalDate.of(2024, 3, 22)
+
+  val Topics = List("Functional Programming", "Use-cases", "Scala Native", "Direct style Scala", "Macros", "Tooling")
